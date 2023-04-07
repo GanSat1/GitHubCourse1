cef26eb6eb9a004f341ce5152f8e0da5364ce5d0
Ganesh11 <ganeshsathya11@gmail.com>
2023-04-07 21:13:40 +0530

    added/updated/deleted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,5 +4,8 @@ public class LoginPage {
 
     public static void main(String[] args) {
         System.out.println("Hello LP");
+        String un = "admin";
+        String pass = "pass";
+        System.out.println("Logged in ......");
     }
 }
