e358b26ee501647465e0ecebd377d969a7bc19bf
Ganesh11 <ganeshsathya11@gmail.com>
2023-04-08 18:38:02 +0530

    Updates in CartPage

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -3,5 +3,6 @@ package pages;
 public class CartPage {
     public static void main(String[] args) {
         System.out.println("Cart Page");
+        System.out.println("Cart Page Feature");
     }
 }
