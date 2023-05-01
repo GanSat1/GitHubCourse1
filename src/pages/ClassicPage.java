9dadf2ede417d09fa2edfd49035e880c8a3a2855
Ganesh11 <ganeshsathya11@gmail.com>
2023-05-01 09:57:53 +0530

    added classic page

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class ClassicPage {
+
+    public void classic(){
+        int n= 20;
+        int n1 = 20;
+        int m = n+n1;
+        System.out.println(m);
+    }
+}
